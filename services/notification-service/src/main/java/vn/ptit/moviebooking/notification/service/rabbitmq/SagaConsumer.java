70565c54bc30e8b94ef671024efdb444b32bd356
thoaidc <dcthoai1023@gmail.com>
2025-08-16 23:04:40 +0700

    add config server config

--- services/notification-service/src/main/java/vn/ptit/moviebooking/notification/service/rabbitmq/SagaConsumer.java
+++ services/notification-service/src/main/java/vn/ptit/moviebooking/notification/service/rabbitmq/SagaConsumer.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 import vn.ptit.moviebooking.notification.constants.NotificationConstants;
 import vn.ptit.moviebooking.notification.constants.RabbitMQConstants;
-import vn.ptit.moviebooking.notification.dto.request.NotificationProcessCommand;
 import vn.ptit.moviebooking.notification.dto.request.NotificationRequest;
-import vn.ptit.moviebooking.notification.dto.response.BaseCommandReplyMessage;
 import vn.ptit.moviebooking.notification.entity.Notification;
 import vn.ptit.moviebooking.notification.service.NotificationService;
 
@@ -31,27 +29,27 @@ public class SagaConsumer {
         this.rabbitMQProducer = rabbitMQProducer;
     }
 
-    @RabbitListener(queues = RabbitMQConstants.Queue.NOTIFICATION_COMMAND)
-    public void handleNotificationRequest(NotificationProcessCommand command, Channel channel, Message amqpMessage) {
-        log.info("[BOOKING] - Notification confirm to customer: {}", command);
-        BaseCommandReplyMessage replyMessage = new BaseCommandReplyMessage();
-        replyMessage.setSagaId(command.getSagaId());
-
-        try {
-            NotificationRequest notificationRequest = command.getNotificationRequest();
-            Notification notification = notificationService.sendNotification(notificationRequest);
-
-            boolean isSentSuccess = Objects.nonNull(notification)
-                    && Objects.equals(NotificationConstants.NotificationStatus.SUCCESS, notification.getStatus());
-            replyMessage.setStatus(isSentSuccess);
-            replyMessage.setResult(notification);
-            log.info("[BOOKING] - Sent notification status: {}", isSentSuccess);
-
-            rabbitMQProducer.confirmProcessed(channel, amqpMessage);
-        } catch (Exception e) {
-            rabbitMQProducer.notifyError(channel, amqpMessage, false);
-        }
-
-        rabbitMQProducer.sendMessage(RabbitMQConstants.RoutingKey.NOTIFICATION_REPLY, replyMessage);
-    }
+//    @RabbitListener(queues = RabbitMQConstants.Queue.NOTIFICATION_COMMAND)
+//    public void handleNotificationRequest(NotificationProcessCommand command, Channel channel, Message amqpMessage) {
+//        log.info("[BOOKING] - Notification confirm to customer: {}", command);
+//        BaseCommandReplyMessage replyMessage = new BaseCommandReplyMessage();
+//        replyMessage.setSagaId(command.getSagaId());
+//
+//        try {
+//            NotificationRequest notificationRequest = command.getNotificationRequest();
+//            Notification notification = notificationService.sendNotification(notificationRequest);
+//
+//            boolean isSentSuccess = Objects.nonNull(notification)
+//                    && Objects.equals(NotificationConstants.NotificationStatus.SUCCESS, notification.getStatus());
+//            replyMessage.setStatus(isSentSuccess);
+//            replyMessage.setResult(notification);
+//            log.info("[BOOKING] - Sent notification status: {}", isSentSuccess);
+//
+//            rabbitMQProducer.confirmProcessed(channel, amqpMessage);
+//        } catch (Exception e) {
+//            rabbitMQProducer.notifyError(channel, amqpMessage, false);
+//        }
+//
+//        rabbitMQProducer.sendMessage(RabbitMQConstants.RoutingKey.NOTIFICATION_REPLY, replyMessage);
+//    }
 }
